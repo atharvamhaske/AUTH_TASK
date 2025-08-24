f20c4dea629da37618c592ccbc7e0d2ae6dc4cda
atharvamhaske <atharvamhaske76@gmail.com>
2025-08-24 21:16:23 +0530

    $*

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,30 @@ app.get("/", async (c) => {
     logger.info("hitted endpoint")
     await loggerErr('this is log to tg bot')
     return c.text('hello from logger ')
+});
+
+app.get("/login", async (c) => {
+    try {
+        logger.info("login attempted");
+        await loggerErr("user tried logging in")
+
+        // on success
+        return c.json({
+            message: "login successful"
+        })
+    } catch (error) {
+        logger.error("login failed", error as Error);
+                await loggerErr(`login failed: ${(error as Error).message}`);
+         return c.json({
+             error: "login failed"
+         }, 500);
+     }
 })
 
-export default app;
+const port = process.env.PORT || 3001;
+
+console.log(`server is running on port ${port}`);
+export default {
+    port,
+    fetch: app.fetch
+};
